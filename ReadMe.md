741032930e877d4a851c4267227b12995f1717d8
owensutton <45274107+owensutton@users.noreply.github.com>
2022-01-19 00:46:26 -0500

    Initial commit

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+**Secure_Storage**
+
+**Version 1.00**
+
+**Description:** Program that allows users to place files into a secure folder that get's encrpyted. 
---
+++ Secure_Storage.py
@@ -0,0 +1,18 @@
+#
+# Program Name: Secure_Storage
+# Original Developer: Owen Sutton
+# Description: This program was developed to create a secure storage folder.
+#              Users will be able to send a file that will be encrypted and store securely.
+#
+#  Developed in Python3.
+#
+# Change Log
+#
+# Version #   Developer     Date of Change   Description of Change
+# ---------   -----------   --------------   ----------------------
+# 1.00        Owen Sutton   01/18/2022       New Program
+
+
+
+
+
